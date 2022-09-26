beb2a77f2d0470c9e47f07f28e1023547017a7f3
Lautaro Garibaldi <lautaro.garibaldi@gmail.com>
2022-09-25 23:36:59 -0300

    Correciones ortograficas

--- scripts/main.js
+++ scripts/main.js
@@ -7,17 +7,17 @@ let precioMemoria = 0;
 const iva = 1.21;
 /*Inicio Definicion componentes PC y funciones para ingresar o cambiar 1 componente*/
 function ingresarProcesador(marca) {
-    let procesadorNuevo = marca.toLowerCase()
+    let procesadorNuevo = marca.toLowerCase();
     while ((procesadorNuevo != "amd") && (procesadorNuevo != "intel")) {
-        procesadorNuevo = prompt("eliga una marca de procesador valida. intel($300) o amd($150)?").toLowerCase();
+        procesadorNuevo = prompt("elija una marca de procesador valida. intel($300) o amd($150)?").toLowerCase();
     }
     if (procesador === procesadorNuevo) {
-        alert("Es el mismo procesador que ya tenias.")
+        alert("Es el mismo procesador que ya tenias.");
     } else {
         if(procesadorNuevo === "intel"){
-            precioProcesador = 300
+            precioProcesador = 300;
         } else{
-            precioProcesador = 150
+            precioProcesador = 150;
         }
         procesador = procesadorNuevo;
         alert("Se agrego un procesador marca " + procesadorNuevo);
@@ -25,17 +25,17 @@ function ingresarProcesador(marca) {
 }
 
 function ingresarPlaca(marca) {
-    let placaNueva = marca.toLowerCase()
+    let placaNueva = marca.toLowerCase();
     while ((placaNueva != "nvidia") && (placaNueva != "amd")) {
-        placaNueva = prompt("eliga una marca de placa de video valida. nvidia($650) o amd($335)?").toLowerCase();
+        placaNueva = prompt("elija una marca de placa de video valida. nvidia($650) o amd($335)?").toLowerCase();
     }
     if (placa === placaNueva) {
-        alert("Es la misma placa de video que ya tenias.")
+        alert("Es la misma placa de video que ya tenias.");
     } else {
         if(placaNueva === "nvidia"){
-            precioPlaca = 650
+            precioPlaca = 650;
         } else{
-            precioPlaca = 335
+            precioPlaca = 335;
         }
         placa = placaNueva;
         alert("se agrego una placa de video marca " + placaNueva);
@@ -45,15 +45,15 @@ function ingresarPlaca(marca) {
 function ingresarMemoria(marca) {
     let memoriaNueva = marca.toLowerCase();
     while ((memoriaNueva != "kingstone") && (memoriaNueva != "segate")) {
-        memoriaNueva = prompt("eliga una marca de memoria ram valida. kingstone($260) o segate($180)?").toLowerCase();
+        memoriaNueva = prompt("elija una marca de memoria ram valida. kingstone($260) o segate($180)?").toLowerCase();
     }
     if (memoria === memoriaNueva) {
-        alert("Es la misma memoria ram que ya tenias.")
+        alert("Es la misma memoria ram que ya tenias.");
     } else {
         if(memoriaNueva === "kingstone"){
-            precioMemoria = 260
+            precioMemoria = 260;
         } else{
-            precioMemoria = 180
+            precioMemoria = 180;
         }
         memoria = memoriaNueva;
         alert("se agrego una memoria ram marca " + memoriaNueva);
@@ -67,45 +67,45 @@ function mostrarPc() {
 
 /*Inicio Funcio para armar una pc*/
 function armarPc(){
-    let procesadorPc = prompt("eliga su marca de procesador. intel ($300) o amd($150)?").toLowerCase();
-    ingresarProcesador(procesadorPc)
-    let placaPc = prompt("eliga su marca de placa de video. nvidia($650) o amd($335)?").toLowerCase();
-    ingresarPlaca(placaPc)
-    let memoriaPc = prompt("eliga su marca de memoria ram. kingstone($260) o segate($180)?").toLowerCase();
-    ingresarMemoria(memoriaPc)
+    let procesadorPc = prompt("elija su marca de procesador. intel ($300) o amd($150)?").toLowerCase();
+    ingresarProcesador(procesadorPc);
+    let placaPc = prompt("elija su marca de placa de video. nvidia($650) o amd($335)?").toLowerCase();
+    ingresarPlaca(placaPc);
+    let memoriaPc = prompt("elija su marca de memoria ram. kingstone($260) o segate($180)?").toLowerCase();
+    ingresarMemoria(memoriaPc);
 }
 /*Fin Funcio para armar una pc*/
 
 /*Inicio Funciones para sacar precio co y sin iva*/
 function precio(){
-    let precioSinIva = 0
+    let precioSinIva = 0;
     if (precioProcesador!= 0 && precioPlaca !=0 && precioMemoria != 0){
-        precioSinIva = precioProcesador + precioPlaca + precioMemoria
-        return(precioSinIva)
+        precioSinIva = precioProcesador + precioPlaca + precioMemoria;
+        return(precioSinIva);
     } else{
-        alert("No se puede saber el precio si no se arma la pc!")
-        return(precioSinIva)
+        alert("No se puede saber el precio si no se arma la pc!");
+        return(precioSinIva);
     }
 }
 
 function precioFinal(){
     let precioFinalConIva = 0;
-    return(precioFinalConIva = (precio()*iva))
+    return(precioFinalConIva = (precio()*iva));
 }
 /*Fin Funciones para sacar precio co y sin iva*/
 /*funcion para comprar pc*/
 function comprarPc(){
     let inputComprar = prompt("Desea armar una computadora? si/no").toLowerCase();
     while(inputComprar != "si" && inputComprar != "no"){
-        alert("Valor ingresado no valido. Ingrese si o no.")
+        alert("Valor ingresado no valido. Ingrese si o no.");
         inputComprar = prompt("Desea armar una computadora? si/no").toLowerCase();
     }
     if(inputComprar ==="si"){
         armarPc();
-        mostrarPc()
+        mostrarPc();
         alert("El precio final de su pc con iva incluido es: " + precioFinal());
     } else {
-        alert("Vayase a cagar.")
+        alert("Gracias, vuelva pronto!.");
     }
 }
-setTimeout(comprarPc,100)
+setTimeout(comprarPc,100);
